2c4c1c40cbe970f9f506f51d2b0857aa72ef8a81
Krishna <krishnapdbashyal23@gmail.com>
2026-01-03 03:52:09 -0500

    feat: added seeding for ticket status and piority

---
+++ BugTracker/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using BugTracker.Models;
+namespace BugTracker.Data
+{
+    // static class casue we dont need to store state, run onetime setup acction. 
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            // Seed TicketStatus
+            if (!context.TicketStatus.Any())
+            {
+                context.TicketStatus.AddRange(
+                    new TicketStatus { Name = "Open" },
+                    new TicketStatus { Name = "In Progress" },
+                    new TicketStatus { Name = "Resolved" },
+                    new TicketStatus { Name = "Closed" }
+                );
+            }
+            // Seed TicketPriority
+            if (!context.TicketPriority.Any())
+            {
+                context.TicketPriority.AddRange(
+                    new TicketPriority { Name = "Low" },
+                    new TicketPriority { Name = "Medium" },
+                    new TicketPriority { Name = "High" },
+                    new TicketPriority { Name = "Urgent" }
+                );
+            }
+            context.SaveChanges();
+        }
+    }
+}
+
