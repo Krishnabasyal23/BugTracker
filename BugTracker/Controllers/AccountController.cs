7aec0011c7d8d7b5f6789408bc51aa789b62e1cd
Krishna <krishnapdbashyal23@gmail.com>
2026-01-07 19:30:04 -0500

    feat: add login page and  navigation features

--- BugTracker/Controllers/AccountController.cs
+++ BugTracker/Controllers/AccountController.cs
@@ -4,7 +4,13 @@ namespace BugTracker.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
         }
