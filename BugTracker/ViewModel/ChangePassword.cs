b8c8c468b79bc44f729bda33ed3177a0e6559c87
Krishna <krishnapdbashyal23@gmail.com>
2026-01-14 19:33:05 -0500

    feat: add change password

--- BugTracker/ViewModel/ChangePassword.cs
+++ BugTracker/ViewModel/ChangePassword.cs
@@ -4,14 +4,16 @@ namespace BugTracker.ViewModel
 {
     public class ChangePassword
     {
+       
+        [EmailAddress]
+        [Display(Name = "Enter Email")]
+        public String Email { get; set; }
+
         [Required(ErrorMessage = "Old Password is required")]
         [DataType(DataType.Password)]
         [Display(Name = "Enter Old Passowrd")]
         public String OldPassword { get; set; }
             
-
-
-
         [Required(ErrorMessage = "Password is required")]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "Please enter at least 8 and at max 20 characters")]
         [DataType(DataType.Password)]
@@ -20,7 +22,7 @@ namespace BugTracker.ViewModel
 
         [Required(ErrorMessage = "Enter the same password again to confirm")]
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = " passwords do not match")]
+        [Compare("NewPassword", ErrorMessage = " passwords do not match")]
         [Display (Name="Confirm New Passowrd")]
         public string ConfirmNewPassword { get; set; }
 
